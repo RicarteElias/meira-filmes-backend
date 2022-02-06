8d7b30ed5b0c7dd1144c7340596309203c199d16
ricarte.elias <ricarte.elias.sousa@gmail.com>
2022-02-06 17:05:43 -0300

    adicionando entidade de nota

---
+++ src/main/java/br/com/meiramovies/model/entity/Nota.java
@@ -0,0 +1,35 @@
+package br.com.meiramovies.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_nota",schema = "meira")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Nota{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @OneToOne()
+    @JoinColumn(name = "id")
+    private Usuario usuario;
+
+    @Column(name = "id_filme")
+    private Integer idFilme;
+
+    @Column(name = "descricao", nullable = false ,length = 255)
+    private String descricao;
+
+    @Column(name = "nota", nullable = false)
+    private Integer nota;
+
+}
